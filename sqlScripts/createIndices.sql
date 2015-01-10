6f67f77d8c55a802daa8429bc54bc2ba340df6c7
MattCordell <matt@snoyowie.com>
2015-01-10 21:00:02 +1000

    Add demograph views fix indexes

--- sqlScripts/createIndices.sql
+++ sqlScripts/createIndices.sql
@@ -2,11 +2,11 @@
 CREATE INDEX idx1 ON AgeDistribution(AgeGroup);
 CREATE INDEX idx2 ON AgeDistribution(proportion);
 
-CREATE INDEX idx1 ON ProportionOfMalesForAge(AgeGroup);
-CREATE INDEX idx2 ON ProportionOfMalesForAge(proportion);
+CREATE INDEX idx3 ON ProportionOfMalesForAge(AgeGroup);
+CREATE INDEX idx4 ON ProportionOfMalesForAge(proportion);
 
-CREATE INDEX Idx1 ON CandidateProblemList(disorderSCTID);
+CREATE INDEX idx5 ON CandidateProblemList(disorderSCTID);
 
-CREATE INDEX Idx1 ON DisorderFilters(disorderSCTID, filter);
-CREATE INDEX idx2 ON DisorderFilters(filter, disorderSCTID);
+CREATE INDEX idx6 ON DisorderFilters(disorderSCTID, filter);
+CREATE INDEX idx7 ON DisorderFilters(filter, disorderSCTID);
 
--- sqlScripts/createTables.sql
+++ sqlScripts/createTables.sql
@@ -4,3 +4,89 @@ DROP TABLE IF EXISTS ProportionOfMalesForAge; CREATE TABLE ProportionOfMalesForA
 DROP TABLE IF EXISTS CandidateProblemList; CREATE TABLE CandidateProblemList(disorderSCTID INTEGER);
 DROP TABLE IF EXISTS DisorderFilters; CREATE TABLE DisorderFilters(disorderSCTID INTEGER, filter TEXT);
 
+
+-- Neonates <1/12
+drop view if exists NeonateProblems;
+create view NeonateProblems AS
+select * from CandidateProblemList
+where disorderSCTID not in
+	(select disorderSCTID from DisorderFilters where filter like 'Neonates%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Fertility%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'MiddleAgedAdults%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Seniors%'
+	);
+
+-- Infants <2
+drop view if exists InfantProblems;
+create view InfantProblems AS
+select * from CandidateProblemList
+where disorderSCTID not in
+	(select disorderSCTID from DisorderFilters where filter like 'Neonates%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Fertility%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'MiddleAgedAdults%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Seniors%'
+
+	);
+
+-- Children < 11
+drop view if exists ChildrenProblems;
+create view ChildrenProblems AS
+select * from CandidateProblemList
+where disorderSCTID not in
+	(select disorderSCTID from DisorderFilters where filter like 'Neonates%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Children%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Fertility%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'MiddleAgedAdults%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Seniors%'
+	);
+
+-- Youth <24
+drop view if exists YouthProblems;
+create view YouthProblems AS
+select * from CandidateProblemList
+where disorderSCTID not in
+	(select disorderSCTID from DisorderFilters where filter like 'Neonates%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Children%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'MiddleAgedAdults%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Seniors%'
+	);
+
+-- MiddleAged < 65
+drop view if exists MiddleAgedProblems;
+create view MiddleAgedProblems AS
+select * from CandidateProblemList
+where disorderSCTID not in
+	(select disorderSCTID from DisorderFilters where filter like 'Neonates%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Children%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Seniors%'
+	);
+
+-- Seniors >= 65
+drop view if exists SeniorsProblems;
+create view SeniorsProblems AS
+select * from CandidateProblemList
+where disorderSCTID not in
+	(select disorderSCTID from DisorderFilters where filter like 'Neonates%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Children%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'Fertility%'
+	 UNION
+	 select disorderSCTID from DisorderFilters where filter like 'MiddleAgedAdults%'
+	);
+
